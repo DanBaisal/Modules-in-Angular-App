aad834673da46a83ab34b27b97a2e8240af49c6d
DanBaisal <dan.baisal92@gmail.com>
2020-03-06 23:07:29 +0100

    Improved the modal, added the accordion feature

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mods-home.component.css']
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'Because it is'},
+    { title: 'What does an orange taste like?', content: 'An orange tastes like an orange'},
+    { title: 'What colour is that cat?', content: 'The cat is an orange colour'}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.modalOpen = !this.modalOpen;
   }
 
 }
